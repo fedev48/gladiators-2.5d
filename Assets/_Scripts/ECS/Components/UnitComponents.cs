a6af569bab720c36e48a03c8aabe076fc259fc89
fedev48 <fede_v@outlook.com>
2026-06-29 14:47:29 +0100

    pre-merge

--- Assets/_Scripts/ECS/Components/UnitComponents.cs
+++ Assets/_Scripts/ECS/Components/UnitComponents.cs
@@ -3,12 +3,16 @@ using Unity.Mathematics;
 
 public struct UnitTag : IComponentData {}
 
+//movement
 public struct MoveDirection : IComponentData { public float3 value; }
 public struct MoveSpeed     : IComponentData { public float  value; }
+public struct NeedsPathfinding : IComponentData, IEnableableComponent { public int flowFieldId; }
 
+
+//animation
 public struct VisualEntity      : IComponentData { public Entity value; }
 public struct FacingDirection   : IComponentData { public float3 value; }
-
 public struct UnitMovementAnimTag : IComponentData {}
-
 public struct OneShotAnimTag : IComponentData, IEnableableComponent {}
+
+
